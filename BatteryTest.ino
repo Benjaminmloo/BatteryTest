e1513bc6ebe551ca8b6f0564453798b1f42a97e4
Benjamin Loo <bloo@eme.ca>
2019-10-09 19:43:28 -0400

    Dynamic Text Size and more
    Updated field locations 
    
    Method Documentation
    
    Move to direct port reading rather than digitalRead(); calls
    
    Smarter encoder change readings
    
    Reformed variable, method and constant names
    
    Moved away from using quit as state variable to just using cursor position
    
    discrete (Riemann) integral implemented for Energy and Charge passed through discharge circuit

--- BatteryTest.ino
+++ BatteryTest.ino
@@ -5,41 +5,53 @@
 
 #define VER "0.0"
 
-//pin values
+/*PINS USED*/
+#define TFT_PIN_RS 11
 #define TFT_PIN_CS 10
-#define TFT_PIN_DC 11
-#define TFT_PIN_RS 9
+#define TFT_PIN_DC 9
 
 #define ENC_PIN_A 0
-#define ENC_PIN_B 13
+#define ENC_PIN_B 6
 #define ENC_PIN_C 1
 
 #define SEN_PIN_V A0
 #define SEN_PIN_C A1
 #define SEN_PIN_REF A5
 
-//sensor reading  values
-#define VREF 3.3 //reference voltage
-#define VDIV 6 //voltage divider compensation factor
+/*SENSOR GAIN & OFFSETS*/
+#define REF_V 3.3 //reference voltage
+#define DIV_V 6 //voltage divider compensation factor
 
-#define VCUR 10 //voltage conversion factor for current sensor
-
-#define CUR_OFFSET (currentSenRef * -1 * VCUR / 2.042)
+#define CUR_V 10 //voltage conversion factor for current sensor
+#define CUR_OFFSET (currentSenRef * -1 * CUR_V / 2.042) //The voltage that the current sensor outputs at 0A
 
 #define ADC_DIV 1024
 
-#define SEN_GAIN_V ((VREF * VDIV) / (ADC_DIV))//range of supply / (range of analog read * max int value)
-#define SEN_GAIN_C ((VREF * VCUR) / ADC_DIV)
+#define SEN_GAIN_V ((REF_V * DIV_V) / ADC_DIV)//range of supply / (range of analog read * max int value)
+#define SEN_GAIN_C ((REF_V * CUR_V) / ADC_DIV)
 
-//screen values
+#define H_PER_uS 2.777777777E-10
+
+/*SCREEN VALUES*/
 #define SCREEN_WIDTH 160 // tft display width, in pixels
 #define SCREEN_HEIGHT 128 // tft display height, in pixels
+#if SCREEN_WIDTH <= 160
+  #define TEXT_SIZE 1
+  #define TEXT_W 6
+  #define TEXT_H 8 
+#else
+  #define TEXT_SIZE 2
+  #define TEXT_W 12
+  #define TEXT_H 16 
+#endif
 
-#define MENU_Y (SCREEN_HEIGHT - 64)
 
-#define MODE_X 108
+#define MENU_Y (SCREEN_HEIGHT - TEXT_H * 4)
+
+#define MODE_X TEXT_W * 9
 #define MODE_Y MENU_Y
 
+//16bit colour values
 #define LTBLUE    0xB6DF
 #define LTTEAL    0xBF5F
 #define LTGREEN   0xBFF7
@@ -78,10 +90,13 @@
 #define DKPURPLE  0x4010
 #define DKGREY    0x4A49
 
-//State variable values
+/*STATE DEFFINITIONS*/
+
+//Operation Mode
 #define CC 0
 #define CP 1
 
+//States
 #define ST_SETUP 0
 #define ST_SET 1
 #define ST_VERIFY 2
@@ -90,6 +105,7 @@
 
 #define NUM_ST 5
 
+//cursor positions
 #define CR_MODE 0
 #define CR_SET_RATE 1
 #define CR_SET_COV 2
@@ -98,7 +114,7 @@
 #define CR_YES CR_START
 #define CR_QUIT CR_START
 
-//Field index value
+//Value array 
 #define I_S_CUR 0
 #define I_S_COV 1
 #define I_S_PWR 2
@@ -107,91 +123,109 @@
 #define I_C_VLT 4
 #define I_C_PWR 5
 
+#define NUM_VALUES 9
+#define NUM_FIELDS 5
+
 #define I_C_CHG 6
 #define I_C_NRG 7 //energy
 
-//Operational values
-#define TICK_LENGTH 100
-#define DEBOUNCE_BTN_DELAY 700
-#define DEBOUNCE_ENC_DELAY 200
-#define SENSOR_UPDATE_T 500
+/*OPERATION VALUES*/
 
+//Timing periods
+#define CR_FLASH_T 100
+#define DEBOUNCE_BTN_T 500
+#define DEBOUNCE_ENC_T 45
+#define SENSOR_PRINT_T 500
+
+//Boundry values
 #define MAX_CURRENT 19.0
 #define SET_STEP 0.1
 
+//Print values
 #define FLOAT_PREC 2
 #define FLOAT_MIN_L 5
 
-#define NUM_VALUES 9
-#define NUM_FIELDS 5
 #define FLD_STR_L 8
 #define POSTFIX_L 3
 
+/*MACROS*/
+#define CLEAR_TEXT_AREA tft.fillRect(0, MENU_Y, SCREEN_WIDTH, SCREEN_HEIGHT - MENU_Y, BLACK)
 
+//Screen deffinition
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_PIN_CS, TFT_PIN_DC, TFT_PIN_RS);
 
-
+//Interupt Flags / Values
 volatile int encDir;
 
-volatile bool newEnc;
+//flag to indicate new values
+volatile bool newEnc; 
 volatile bool newBtn;
 
+//enables for inputs
 volatile bool enEnc;
 volatile bool enBtn;
 
-
-byte state;
-byte cursorPos;
-byte lastCursorPos;
-
-int cursorX[] = {0, 0, 0, 0, 48};
-int cursorY[] = {MENU_Y, MENU_Y + 16, MENU_Y + 32, MENU_Y + 48, MENU_Y + 48};
-
-bool mode;
-bool viewMode;
-bool displayedMode;
-char modeStr[][3] = {"CC", "CP"};
-
-bool setSel;
-bool quit;
-bool negCurrent;
-bool redraw;
-bool showCursor;
-bool printSen;
-
-float currentSenRef;
-float value[NUM_VALUES];
-char valuePostfix[][3] = {"A", "V", "W", "A", "V", "W", "Ah", "Wh"};
-byte valueField[] = {1, 2, 1, 3, 4, 3, 0, 0};
-
-
-char fieldLastWr[NUM_FIELDS][FLD_STR_L];
-int fieldX[] = {12, 12, 12, 84, 84};
-int fieldY[] = { MENU_Y, MENU_Y + 16, MENU_Y + 32, MENU_Y + 16, MENU_Y + 32};
-
-float checkVal;
-float ox, oy;
-
-unsigned long lastTick;
-unsigned long lastUpdate;
+//time of last update
 volatile unsigned long lastBtn;
 volatile unsigned long lastEnc;
 
+//State Variables
+byte state;
+byte cursorPos;
+byte lastCursorPos;
+byte initEncState;
+
+bool mode;
+bool viewMode; // mode being viewed on run 
+bool displayedMode; //last mode displayed on screen
+bool initialSenRead;
+
+
+/*ARRAYS FOR VALUES*/
+char modeStr[][3] = {"CC", "CP"};
+
+int cursorX[] = {0, 0, 0, 0, TEXT_W * 5};
+int cursorY[] = {MENU_Y, MENU_Y + TEXT_H, MENU_Y + TEXT_H * 2, MENU_Y + TEXT_H * 3, MENU_Y + TEXT_H * 3};
+
+//Values
+double value[NUM_VALUES]; //storage for values
+char valuePostfix[][3] = {"A", "V", "W", "A", "V", "W", "Ah", "Wh"}; //units for respective values
+byte valueField[] = {1, 2, 1, 3, 4, 3, 0, 0}; //field that each value belogns to
+
+float currentSenRef; //Reference internal use only
+
+//field - positions on the screen where values are written
+char fieldLastWr[NUM_FIELDS][FLD_STR_L]; //The last string printed to a given field - used to quickly clear that field for the next value
+int fieldX[] = {TEXT_W, TEXT_W, TEXT_W, TEXT_W * 8, TEXT_W * 8}; 
+int fieldY[] = { MENU_Y, MENU_Y + TEXT_H, MENU_Y + TEXT_H * 2, MENU_Y + TEXT_H, MENU_Y + TEXT_H * 2};
+
+//non-volatile timing variables
+unsigned long lastCursorFlash;
+unsigned long lastSensorPrint;
+unsigned long lastSensorCheck;
+bool printSen;
+bool showCursor;
+
+
+float checkVal;
+float ox, oy;
+bool redraw;
 
 void setup() {
   Serial.begin(9600);
   tft.initR(INITR_BLACKTAB);
 
   tft.setRotation(3);
+  tft.setTextWrap(false);
 
   tft.setTextColor(WHITE);
-  tft.setTextSize(0x02);
+  tft.setTextSize(TEXT_SIZE);
   tft.fillScreen(BLACK);
 
   printFlash();
 
   //initiallize timers
-  lastTick = 0;
+  lastCursorFlash = 0;
   lastBtn = 0;
   lastEnc = 0;
   lastCursorPos = 0;
@@ -229,7 +263,19 @@ void setup() {
 
   initSetupDisplay();
 }
-
+/*
+ * Main Loop
+ * 
+ * General programtic structure is as follows
+ * 
+ * Accept interupts which asyncronously set flags for main loop
+ * 
+ * Given the current state check if a flag has been set make state/value changes accordingly 
+ * if menu is changed update initialise the new screen\
+ * if the cursor is moved repirint it in the new location
+ * 
+ * when the main loop is completed check the time to 
+ */
 void loop() {
   switch (state) {
     case ST_SETUP: //while in setup
@@ -251,7 +297,6 @@ void loop() {
             break;
           case CR_START: //if cursor is on start change to that state and reset cursor
             state = ST_VERIFY;
-            quit = 0;
             initVerifyDisplay();
             break;
         }
@@ -350,7 +395,6 @@ void loop() {
       if (newBtn) {
         newBtn = false;
         state = ST_QUIT;
-        quit = 0;
 
         initQuitDisplay();
       }
@@ -400,101 +444,153 @@ void loop() {
   checkSensors();
 }
 
+
+/*
+ * encoder interupt handler
+ * 
+ * waits for an interupt on pinA
+ * then looks for and edge on pinB by 
+ * accepting all of the pinA interupts that are seen for a single encoder tick 
+ * and looking for a change in value on pinB
+ * 
+ * when a change of value is detected stop looking for a change 
+ * until the next interupt that takes longer than the set DEBOUNCE_ENC_T
+ */
 void doEncoder() {
-  unsigned long curTime = millis();
-  if(enEnc){
-    if (digitalRead(ENC_PIN_A) == digitalRead(ENC_PIN_B)) {
-      encDir = -1;
-    } else {
-      encDir = 1;
-    }
-    Serial.println(encDir);
-    newEnc = true;
-    enEnc = false;
-    lastEnc = curTime;
+  cli();
+  unsigned long curTime, timeDif;
+  byte pinState = PIND & B10000000;
+  curTime = millis();
+  timeDif = curTime - lastEnc;
+    
+  if(curTime - lastEnc < DEBOUNCE_ENC_T){    
+    if(enEnc){
+      if(initEncState != pinState){
+        newEnc = true;
+        encDir = initEncState? -1 : 1;
+        enEnc = false;
+      }
+    } 
+    
+  } else {
+    initEncState = pinState & B10000000;
+    enEnc = true;
   }
+  
+  
+  lastEnc = curTime;
+  sei();
 }
 
+/*
+ * Button interupt handeler
+ */
 void doButton() {
-  unsigned long curTime = millis();
-  if (enBtn) {
-    enBtn = false;
-    if (digitalRead(ENC_PIN_C == 0)) {
-      newBtn = true;
-      lastBtn = curTime;
-    }
+  unsigned long curTime;
+  cli();
+  curTime = millis();
+  
+  if (curTime > lastBtn + DEBOUNCE_BTN_T) {
+    newBtn = true;
+    lastBtn = curTime;
   }
+  sei();
 }
 
+/*
+ * time management function
+ * 
+ * Updatees time sensetive flags not otherwise handled in interupts
+ */
 void checkTime() {
   unsigned long curTime = millis();
 
-  if (curTime > lastTick + TICK_LENGTH) {
-    lastTick = curTime;
+  if (curTime > lastCursorFlash + CR_FLASH_T) {
+    lastCursorFlash = curTime;
     showCursor = !showCursor;
   }
 
-  if (curTime > lastUpdate + SENSOR_UPDATE_T) {
-    lastUpdate = curTime;
+  if (curTime > lastSensorPrint + SENSOR_PRINT_T) {
+    lastSensorPrint = curTime;
     printSen = true;
   }
 
-  if (!enBtn && curTime > lastBtn + DEBOUNCE_BTN_DELAY) {
-    lastBtn = curTime;
-    enBtn = true;
-  }
-
-  if (!enEnc && curTime > lastEnc + DEBOUNCE_ENC_DELAY) {
-    lastEnc = curTime;
-    enEnc = true;
-  }
 }
 
+/*
+ * updates values dependant on sensor readings
+ * 
+ * Reads analog pins that pretain to current and voltage of the discharge circuit
+ * as well as the reference voltage for the current sensor
+ * 
+ * decides whether or not to print the values to the screen dependant on time and current state
+ * 
+ * if the discharge circuit is active calculate how much charge and energy was passed through
+ * the discharge circuit by multiplying the instantanious current / power by the time elapsed
+ * and a time conversion factor
+ * 
+ * resets flags and updates time
+ */
 void checkSensors() {
+  unsigned long curTime = micros();
+  unsigned long timeDif = curTime - lastSensorCheck;
+  
+  currentSenRef = analogRead(SEN_PIN_REF) * SEN_GAIN_V;
   float readCurrent = analogRead(SEN_PIN_C) * SEN_GAIN_C + CUR_OFFSET;
   float readVoltage = analogRead(SEN_PIN_V) * SEN_GAIN_V;
-
+  
   bool doPrintCurrent = printSen && state == ST_RUN && viewMode == CC;
   bool doPrintPower = printSen && state == ST_RUN && viewMode == CP;
   bool doPrintVoltage = printSen && state < NUM_ST && state != ST_QUIT;
 
-
   updateValue(I_C_CUR, readCurrent, doPrintCurrent);
   updateValue(I_C_VLT, readVoltage, doPrintVoltage);
 
-  updateValue(I_C_PWR, readVoltage * readCurrent, doPrintPower);
+  updateValue(I_C_PWR, readVoltage * readCurrent, doPrintPower); 
+  
+  if(!initialSenRead && state == ST_RUN){
+    value[I_C_CHG] += value[I_C_CUR] * timeDif * H_PER_uS;
+    value[I_C_NRG] += value[I_C_PWR] * timeDif * H_PER_uS;
+  }
 
 
-  currentSenRef = analogRead(SEN_PIN_REF) * SEN_GAIN_V;
+  initialSenRead = false;
   printSen = false;
+  lastSensorCheck = curTime;
 }
 
+/*
+ * Updates the cursor postion on the screen
+ * 
+ * overwrites the last position with the background colour
+ * writes the cursor in at the new position
+ */
 void updateCursor(){
-  tft.setCursor(cursorX[lastCursorPos], cursorY[lastCursorPos]);
-  tft.setTextColor(BLACK);
-  tft.setTextSize(0x02);
-  tft.print('>');
+  printString('>', cursorX[lastCursorPos], cursorY[lastCursorPos], BLACK);
 
-  tft.setCursor(cursorX[cursorPos], cursorY[cursorPos]);
-  tft.setTextColor(WHITE);
-  tft.print('>');
-
+  printString('>', cursorX[cursorPos], cursorY[cursorPos], WHITE);
   lastCursorPos = cursorPos;
 }
 
+
+/*
+ * Prints some string to a given location
+ */
 void printString(char * str, int x, int y, int color) {
   tft.setCursor(x, y);
   tft.setTextColor(color);
-  tft.setTextSize(0x02);
+  tft.setTextSize(TEXT_SIZE);
 
   tft.print(str);
 }
 
 /*
-   Updates value in field array
-   if specified will erase last value written to screen and write the updated value
+ * Updates value in value array
+ * ARGS: v - index of value to update
+ *       newValue - the value to be written
+ *       printUpdate - whether or not to write the new value
 */
-void updateValue(byte v, float newValue, bool printUpdate) {
+void updateValue(byte v, double newValue, bool printUpdate) {
   byte field = valueField[v];
   value[v] = newValue;
 
@@ -505,10 +601,10 @@ void updateValue(byte v, float newValue, bool printUpdate) {
 }
 
 /*
-   Renders field string based on current field value and prints to set location on screen
-   stores last written field string for possible erasure
-
-   ARGS: byte f - position of field in data array
+ * Renders field string based on current field value and prints to set location on screen
+ * stores last written field string for possible erasure
+ * 
+ * ARGS: byte v - Index of value to be printed
 */
 void printValue(byte v) {
   byte field = valueField[v];
@@ -518,44 +614,40 @@ void printValue(byte v) {
   printString(fieldLastWr[field], fieldX[field], fieldY[field], WHITE);
 }
 
+/*
+ * Refreshes text area and writes flash screen information
+ */
 void printFlash() {
-  tft.setCursor(12, MENU_Y);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(0x02);
-
-  tft.print("BATT TEST\n v");
+  printString("BATT TEST\n v", TEXT_W, MENU_Y, WHITE);
   tft.print(VER);
 }
 
+/*
+ * Prints the current mode to the screen
+ * 
+ * over write the last written mode with the background colour
+ */
 void printMode() {
-  tft.setCursor(MODE_X, MODE_Y);
-  tft.setTextColor(BLACK);
-  tft.setTextSize(0x02);
-
-  tft.print(modeStr[displayedMode]);
-
-  tft.setCursor(MODE_X, MODE_Y);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(0x02);
-
-  tft.print(modeStr[mode]);
+  printString(modeStr[displayedMode], MODE_X, MODE_Y, BLACK);
+  
+  printString(modeStr[mode], MODE_X, MODE_Y, WHITE);
   displayedMode = mode;
 }
 
+/*
+ * Initialises the setup screen 
+ * 
+ * Sets some state variables as well as prints the menu's fields
+ */
 void initSetupDisplay() {
-  tft.fillRect(0, MENU_Y, SCREEN_WIDTH, SCREEN_HEIGHT - 10, BLACK);
+  CLEAR_TEXT_AREA;
   
   cursorPos = CR_MODE;
   updateCursor();
 
-  tft.setCursor(12, MENU_Y);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(0x02);
-
-  tft.print("MODE:  ");
+  printString("MODE:  ", TEXT_W * 2, MENU_Y, WHITE);
   
-  tft.setCursor(12, MENU_Y + 48);
-  tft.print("START \n");
+  printString("START \n", TEXT_W * 2, MENU_Y + TEXT_H * 3, WHITE);
 
   printMode();
 
@@ -568,22 +660,21 @@ void initSetupDisplay() {
   printValue(I_S_COV);
   printValue(I_C_VLT);
 
-
-
   updateCursor();
 }
 
+/*
+ * Initialises the verifying screen 
+ * 
+ * Sets some state variables as well as prints the menu's fields
+ */
 void initVerifyDisplay() {
-  tft.fillRect(0, MENU_Y, SCREEN_WIDTH, SCREEN_HEIGHT - MENU_Y, BLACK);
+  CLEAR_TEXT_AREA;
   
   cursorPos = CR_NO;
   updateCursor();
   
-  tft.setCursor(12, MENU_Y);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(0x02);
-
-  tft.print(" START? ");
+  printString("START? ", TEXT_W * 2, MENU_Y , WHITE);
 
   printMode();
 
@@ -595,25 +686,26 @@ void initVerifyDisplay() {
 
   printValue(I_S_COV);
   printValue(I_C_VLT);
-
-
-  tft.setCursor(12, MENU_Y + 48);
-  tft.print("Y   N  ");
+  
+  printString("Y    N  ", TEXT_W * 2, MENU_Y + TEXT_H * 3, WHITE);
 }
 
+
+/*
+ * Initialises the run screen 
+ * 
+ * Sets some state variables as well as prints the menu's fields
+ */
 void initRunDisplay() {
   viewMode = mode;
   value[I_C_CHG] = 0;
   value[I_C_NRG] = 0;
-  
-  tft.fillRect(0, MENU_Y, SCREEN_WIDTH, SCREEN_HEIGHT - MENU_Y, BLACK);
+  initialSenRead= true;
+  CLEAR_TEXT_AREA;
   
   cursorPos = CR_QUIT;
   updateCursor();
   
-  tft.setCursor(12, MENU_Y);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(0x02);
   printMode();
 
   if (viewMode == CC) {
@@ -631,57 +723,58 @@ void initRunDisplay() {
   printValue(I_S_COV);
   printValue(I_C_VLT);
 
-
-  tft.setCursor(12, MENU_Y + 48);
-  tft.print("STOP");
+  printString("STOP", TEXT_W * 2, MENU_Y + TEXT_H * 3, WHITE);
 }
 
+
+/*
+ * Initialises the quit verification screen 
+ * 
+ * Sets some state variables as well as prints the menu's fields
+ */
 void initQuitDisplay() {
-  tft.fillRect(0, MENU_Y, SCREEN_WIDTH, SCREEN_HEIGHT - MENU_Y, BLACK);
+  CLEAR_TEXT_AREA;
   cursorPos = CR_NO;
   updateCursor();
+ 
+  printString("STOP?", TEXT_W, MENU_Y, WHITE);
 
-  tft.setCursor(12, MENU_Y);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(0x02);
-
-  tft.print(" STOP?");
-
-  tft.setCursor(12, MENU_Y + 48);
-  tft.print("Y   N  ");
+  printString("Y    N  ", TEXT_W * 2, MENU_Y + TEXT_H * 3, WHITE);
 }
 
 /*
-   Graph: uses adafruit TFT drive library to draw a cartesian coordinate system and plot whatever data you want
-   just pass x and y and the graph will be drawn
-
-   ARGS:
-     &d name of your display object
-     x = x data point
-     y = y datapont
-     gx = x graph location (lower left)
-     gy = y graph location (lower left)
-     w = width of graph
-     h = height of graph
-     xlo = lower bound of x axis
-     xhi = upper bound of x asis
-     xinc = division of x axis (distance not count)
-     ylo = lower bound of y axis
-     yhi = upper bound of y asis
-     yinc = division of y axis (distance not count)
-     title = title of graph
-     xlabel = x asis label
-     ylabel = y asis label
-     gcolor = graph line colors
-     acolor = axi ine colors
-     pcolor = color of your plotted data
-     tcolor = text color
-     bcolor = background color
-     &redraw = flag to redraw graph on fist call only
-
-   RETURN: void
-   Author:kris kasprzak - https://www.youtube.com/watch?v=YejRbIKe6e0
-*/
+ * Prints graph to screen
+ * 
+ * uses adafruit TFT drive library to draw a cartesian coordinate system and plot whatever data you want
+ * just pass x and y and the graph will be drawn
+ *
+ * ARGS:
+ *   &d name of your display object
+ *   x = x data point
+ *   y = y datapont
+ *   gx = x graph location (lower left)
+ *   gy = y graph location (lower left)
+ 8   w = width of graph
+ *   h = height of graph
+ *   xlo = lower bound of x axis
+ *   xhi = upper bound of x asis
+ *   xinc = division of x axis (distance not count)
+ *   ylo = lower bound of y axis
+ *   yhi = upper bound of y asis
+ *   yinc = division of y axis (distance not count)
+ *   title = title of graph
+ *   xlabel = x asis label
+ *   ylabel = y asis label
+ *   gcolor = graph line colors
+ *   acolor = axi ine colors
+ *   pcolor = color of your plotted data
+ *   tcolor = text color
+ *   bcolor = background color
+ *   &redraw = flag to redraw graph on fist call only
+ *
+ * RETURN: void
+ * Author: Kris Kasprzak - https://www.youtube.com/watch?v=YejRbIKe6e0
+ */
 
 void Graph( Adafruit_ST7735 &d, double x, double y, double gx, double gy, double w, double h, double xlo, double xhi, double xinc, double ylo, double yhi, double yinc, String title, String xlabel, String ylabel, unsigned int gcolor, unsigned int acolor, unsigned int pcolor, unsigned int tcolor, unsigned int bcolor, boolean &redraw) {
   double ydiv, xdiv;
